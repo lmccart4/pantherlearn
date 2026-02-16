ae50dc807aa46301c21d860831071672a99ec21b
lmccart4 <lmccart4@gmail.com>
2026-02-16 14:02:41 -0500

    Auto-convert Google Drive URLs in ImageBlock

--- src/components/blocks/ImageBlock.jsx
+++ src/components/blocks/ImageBlock.jsx
@@ -1,15 +1,35 @@
 // src/components/blocks/ImageBlock.jsx
 import { useTranslatedText } from "../../hooks/useTranslatedText.jsx";
 
+// Convert Google Drive sharing URLs to direct image URLs
+function normalizeImageUrl(url) {
+  if (!url) return url;
+
+  // Google Drive sharing link: https://drive.google.com/file/d/FILE_ID/view?...
+  const driveMatch = url.match(/drive\.google\.com\/file\/d\/([^/]+)/);
+  if (driveMatch) {
+    return `https://drive.google.com/uc?export=view&id=${driveMatch[1]}`;
+  }
+
+  // Google Drive open link: https://drive.google.com/open?id=FILE_ID
+  const openMatch = url.match(/drive\.google\.com\/open\?id=([^&]+)/);
+  if (openMatch) {
+    return `https://drive.google.com/uc?export=view&id=${openMatch[1]}`;
+  }
+
+  return url;
+}
+
 export default function ImageBlock({ block }) {
   const translatedCaption = useTranslatedText(block.caption);
+  const imgUrl = normalizeImageUrl(block.url);
 
   if (!block.url) return null;
 
   return (
     <div style={{ margin: "24px 0" }}>
       <img
-        src={block.url}
+        src={imgUrl}
         alt={block.alt || translatedCaption || "Lesson image"}
         style={{
           width: "100%",
