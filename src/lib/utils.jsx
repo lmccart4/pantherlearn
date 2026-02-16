75d25110e24e3ee4930feb6d28bf8a2f074223d9
lmccart4 <lmccart4@gmail.com>
2026-02-16 12:03:05 -0500

    Security: fix XSS vulnerability in renderMarkdown by escaping HTML

--- src/lib/utils.jsx
+++ src/lib/utils.jsx
@@ -1,7 +1,18 @@
 // src/lib/utils.js
+
+function escapeHTML(text) {
+  return text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
 export function renderMarkdown(text) {
   if (!text) return "";
-  return text
+  // Escape HTML first to prevent XSS, THEN apply markdown formatting
+  return escapeHTML(text)
     .replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>")
     .replace(/\*(.+?)\*/g, "<em>$1</em>")
     .replace(/\n/g, "<br/>");
